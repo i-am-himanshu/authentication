c12634cdc73517abbc3b7ab9d0928f4571ecadbf
himanshu <himanshu.anand.5300@gmail.com>
2026-09-08 02:49:57 +0530

    created user dtos

---
+++ src/main/java/com/himanshu/authentication/dto/UserResponse.java
@@ -0,0 +1,39 @@
+package com.himanshu.authentication.dto;
+
+import com.himanshu.authentication.model.Role;
+
+public class UserResponse {
+
+    private Long id;
+
+    private String username;
+
+    private Role role;
+
+    public UserResponse() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}

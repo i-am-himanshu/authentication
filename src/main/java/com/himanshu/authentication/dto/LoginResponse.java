842d20914c07a5661e65e9a19a5f9eddbc9ac1a1
himanshu <himanshu.anand.5300@gmail.com>
2026-09-09 02:49:20 +0530

    login details dto

---
+++ src/main/java/com/himanshu/authentication/dto/LoginResponse.java
@@ -0,0 +1,17 @@
+package com.himanshu.authentication.dto;
+
+public class LoginResponse {
+
+    private String message;
+
+    public LoginResponse() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}

0ad7319750be727fc7ed806a62df1325b87d6d7d
himanshu <himanshu.anand.5300@gmail.com>
2026-09-25 03:35:36 +0530

    authenticate with token

--- src/main/java/com/himanshu/authentication/security/JwtAuthenticationFilter.java
+++ src/main/java/com/himanshu/authentication/security/JwtAuthenticationFilter.java
@@ -4,13 +4,22 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    public JwtAuthenticationFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
 
 
     @Override
@@ -21,9 +30,22 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
             String token = authorizationHeader.substring(7);
 
-            System.out.println("JWT received: " + token);
+            String username = jwtService.extractUsername(token);
+
+            UsernamePasswordAuthenticationToken authentication =
+                    new UsernamePasswordAuthenticationToken(
+                            username,   //username
+                            null,       // password
+                            new ArrayList<>()       //authorities = roles/permissions
+                    );
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            System.out.println("Username from JWT: " + username);
         }
 
         filterChain.doFilter(request, response);
     }
+
+
 }
